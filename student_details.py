8b17933354c2d75f681fa1761c5f1509cb1dc8fd
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:57 +0530

    Add files via upload

---
+++ student_details.py
@@ -0,0 +1,8 @@
+Name=input("Enter the student Name:")
+Roll_No=int(input("Enter the students Roll No:"))
+Roll_No+=1
+CGP=float(input("Enter the students CGP:"))
+Percentage=CGP*10
+print("Name of the student:",Name)
+print("Roll No of the student:",Roll_No)
+print("The students percentage is",Percentage,"%")
