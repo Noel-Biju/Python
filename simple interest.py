b2de6ec5e324bdf658fa66c6a4c091f6a3e9d259
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-10-15 14:17:41 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,5 @@
+amt=int(input("Enter the amount:"))
+rate=int(input("Enter the rate:"))
+years=int(input("Enter the no of years:"))
+simple_intrest=amt*rate*years
+print("The simple interest is",simple_intrest)
