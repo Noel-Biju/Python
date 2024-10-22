d9cbc0b287310c9a621c80a836138789579edb22
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-10-22 14:26:15 +0530

    Add files via upload

---
+++ temperature conversion.py
@@ -0,0 +1,11 @@
+temp=int(input("Enter Temperature:"))
+a=input("Is this in (C)elsius or (F)ahrenheit?")
+if a=='C':
+    f=(9/5)*temp+32
+    print(temp,"Celsius is", f,"Fahrenheit")
+elif a=='F':
+    c=(5/9)*(temp-32)
+    print(temp,"Fahrenheit is",c,"Celsius")
+else:
+    print("Invalid Temperature")
+
