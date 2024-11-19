a3c9c9f3b0bcfcf6ca80956dba37cd44ae4bc03b
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-11-19 16:19:58 +0530

    Add files via upload

--- Employee management system.py
+++ Employee management system.py
@@ -10,5 +10,5 @@ for employee in employees:
     employee_name,department,monthly_salary=employee
     if monthly_salary>salary_threshold:
         print(employee)
-    total_annual_expense+=monthly_salary
+    total_annual_expense+=monthly_salary*12
 print(f"The total annual payroll expense is {total_annual_expense}")
