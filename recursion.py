3fe94197898ff97c46642cc4c78e471a0d3ad719
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-12-03 10:26:13 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,21 @@
+#Factorial of a number
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
+# Adding two positive numbers
+def add_numbers(n1,n2):
+    if n2==0:
+        return n1
+    else:
+        return add_numbers(n1+1,n2-1)
+print(add_numbers(5,4))
+#Multiplying two numbers
+def multiply(n1,n2):
+    if n2==1:
+        return n1
+    else:
+        return n1+multiply(n1,n2-1)
+print(multiply(5,4))
